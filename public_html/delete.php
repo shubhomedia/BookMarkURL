cad1404d4703ae79d7e4b45dbf4f1358c640bbea
shubhobd <shubhomedia@gmail.com>
2016-02-16 21:03:04 +0600

    Delete Function added

--- public_html/delete.php
+++ public_html/delete.php
@@ -1,10 +1,10 @@
 <?php
 
-    include_once("../../../vendor/autoload.php");
+    include_once("./vendor/autoload.php");
 
     //      session_start();
-    use App\Bitm\SEIP106508\Url\Bookmark;
-    use App\Bitm\SEIP106508\Utility\Utility;
+    use App\Url\Bookmark;
+    use App\Utility\Utility;
 
     $bookmark = new Bookmark();
     $bookmark->delete($_REQUEST['id']);
